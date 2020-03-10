6c06245ccb59f8bf8fda51dfabfe306bacde076e
Keith <kmson7@gmail.com>
2020-03-10 14:53:06 +0800

    created 16x16 grid using JS loops

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const container = document.querySelector('.container');
 
 
-
 for (let i = 0; i < 16; i++) {
-    const square = document.createElement('div');
-    square.textContent = 'hello';
-    container.appendChild(square);        
+    const row = document.createElement('div');
+    row.classList.add('row');
+    container.appendChild(row); 
+
+    for (let j = 0; j < 16; j++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.textContent = '';
+        row.appendChild(square);        
+    }
 }
-
-
 
