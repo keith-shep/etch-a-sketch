8866b8f865357b5b215c835222cc0ac634134df1
Keith <kmson7@gmail.com>
2020-03-11 20:53:43 +0800

    implemented reset button with prompt from user

--- script.js
+++ script.js
@@ -1,39 +1,54 @@
 const body = document.querySelector('body');
 const container = document.querySelector('.container');
 const button = document.createElement('button');
-button.textContent = 'reset'
+button.textContent = 'New Grid'
+button.addEventListener('click', reset);
 body.insertBefore(button, container)
 
-// Create grid
 
-gridSize = prompt('Enter grid size: ','16');
-
-for (let i = 0; i < gridSize; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
-    row.textContent= '';
-    container.appendChild(row); 
-
-    for (let j = 0; j < gridSize; j++) {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        square.textContent = '';
-        row.appendChild(square);        
+function makeGrid(gridSize) {
+    for (let i = 0; i < gridSize; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        row.textContent= '';
+        container.appendChild(row); 
+        
+        for (let j = 0; j < gridSize; j++) {
+            const square = document.createElement('div');
+            square.classList.add('square');
+            square.textContent = '';
+            row.appendChild(square);        
+        }
     }
+    
 }
 
-const squares = document.querySelectorAll('.square');
-
-
-squares.forEach(element =>
-    element.addEventListener('mouseover', () => element.style.backgroundColor = "black")
-);
-
-
-
-button.addEventListener('click', resetColor);
-
-
-function resetColor() {
-    squares.forEach(element => element.style.backgroundColor = 'gray');
+function squareSelector() {
+    squares = document.querySelectorAll(".square");
 }
+
+let removeGrid = () => (container.innerHTML = '');
+
+
+function reset() {
+    gridSize = prompt('Enter grid size: ', '16');
+    removeGrid();
+    squareSelector();
+    makeGrid(gridSize);
+    squareSelector();
+    squares.forEach(element =>
+        element.addEventListener('mouseover', () => element.style.backgroundColor = "black")
+    );
+}
+
+function defaultGrid() {
+    makeGrid(16); 
+    squareSelector();   
+    squares.forEach(element =>
+        element.addEventListener('mouseover', () => element.style.backgroundColor = "black")
+    );
+}
+
+defaultGrid();
+
+
